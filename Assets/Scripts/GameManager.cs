66b3cb8beda224efa1371d48f6527a6cbff174bb
Corentin Couasnon <ccouasno@gmail.com>
2024-02-28 14:59:13 +0100

    Added all game over conditions, move MovePiece method to Piece.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -23,32 +23,17 @@ public class GameManager : MonoBehaviour
                 yield return StartCoroutine(_player1.Play(context));
             else
                 yield return StartCoroutine(_player2.Play(context));
-
-            if (IsGameOver())
+            
+            if (IsGameOver(context))
                 break;
             
             context.IsFirstPlayerTurn = !context.IsFirstPlayerTurn;
         }
 
-        Debug.Log("Winner is " + (context.IsFirstPlayerTurn ? "Player 1" : "Player 2") + " !");
-    }
-
-    public void MovePiece(Piece piece, Vector2 newPosition)
-    {
-        // Déplacement
-        piece.Position = newPosition;
-
-        // Capture
-        var capturedPiece = _allPieces.FirstOrDefault(piece => piece.Position == newPosition);
-        if (capturedPiece != null)
-        {
-            capturedPiece.IsCaptured = true;
-            return;
-        }
-
-        // Parachutage
-        if (piece.IsCaptured)
-            piece.IsCaptured = false;
+        if (!IsThreeFoldRepetition(context))
+            Debug.Log("Winner is " + (context.IsFirstPlayerTurn ? "Player 1" : "Player 2") + " !");
+        else
+            Debug.Log("Game ends in a draw !");
     }
     
     [ContextMenu("Play Game")]
@@ -62,9 +47,62 @@ public class GameManager : MonoBehaviour
         return new GameContext(this, _allPieces, _player1Pieces, _player2Pieces);
     }
 
-    bool IsGameOver()
+    bool IsGameOver(GameContext context)
+    {
+        var anyKoropokkuruCaptured = AnyKoropokkuruCaptured(context);
+        var anyKoropokkuruPromoted = AnyKoropokkuruPromotedAndSafe(context);
+        var threeFoldRepetition = IsThreeFoldRepetition(context);
+
+        return anyKoropokkuruCaptured || anyKoropokkuruPromoted || threeFoldRepetition;
+    }
+
+    bool AnyKoropokkuruCaptured(GameContext context)
     {
         return _allPieces.Where(piece => piece.Type == PiecesType.Koropokkuru).Any(piece => piece.IsCaptured);
+    }
+
+    bool AnyKoropokkuruPromotedAndSafe(GameContext context)
+    {
+        return context.AllPieces
+            .Where(piece => piece.Type == PiecesType.Koropokkuru)
+            .Any(IsKoropokkuruPromotedAndSafe);
+
+        bool IsKoropokkuruPromotedAndSafe(Piece koropokkuru)
+        {
+            var isPlayer1Piece = context.Player1Pieces.Contains(koropokkuru);
+            var promotingRow = isPlayer1Piece ? 3 : 0;
+            var opponentPieces = isPlayer1Piece ? context.Player2Pieces : context.Player1Pieces;
+            
+            if (koropokkuru.IsCaptured || !Mathf.Approximately(koropokkuru.Position.y, promotingRow))
+                return false;
+
+            return opponentPieces
+                .All(opponentPiece => 
+                    !opponentPiece
+                        .GetAllowedMoves(context)
+                        .Contains(koropokkuru.Position)
+                    );
+        }
+    }
+
+    bool IsThreeFoldRepetition(GameContext context)
+    {
+        /*
+         * e4  ^8
+         * e5  ^7
+         * Nf3 ^6
+         * Nf6 ^5
+         * Ng1 ^4
+         * Ng8 ^3
+         * Nf3 ^2
+         * Nf6 ^1
+         * => Draw
+         */
+        
+        if (context.Actions.Count < 6)
+            return false;
+
+        return context.Actions[^1] == context.Actions[^5] && context.Actions[^2] == context.Actions[^6];
     }
 
     void ResetAllPieces()
@@ -82,11 +120,13 @@ public class GameContext
     public IReadOnlyList<Piece> AllPieces { get; }
     public IReadOnlyList<Piece> Player1Pieces { get; }
     public IReadOnlyList<Piece> Player2Pieces { get; }
-    public bool IsFirstPlayerTurn { get; set; } = true;
     
     public IReadOnlyList<Piece> OwnPieces => IsFirstPlayerTurn ? Player1Pieces : Player2Pieces;
     public IReadOnlyList<Piece> OpponentPieces => IsFirstPlayerTurn ? Player2Pieces : Player1Pieces;
     
+    public bool IsFirstPlayerTurn { get; set; } = true;
+    public List<(Piece piece, Vector2 position)> Actions { get; set; } = new List<(Piece piece, Vector2 position)>();
+
     public GameContext(GameManager gameManager, IReadOnlyList<Piece> allPieces, IReadOnlyList<Piece> player1Pieces, IReadOnlyList<Piece> player2Pieces)
     {
         GameManager = gameManager;
